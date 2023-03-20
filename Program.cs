8275b4b88c3a6f685654f6f10f5e99c8d15c87c5
Volodymyr Kushchev <volodymyr.kushchev@gmail.com>
2023-03-20 16:23:01 +0100

    Added errors handling

--- Program.cs
+++ Program.cs
@@ -2,38 +2,16 @@ using Platform.Middelwares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(opts => {
-    opts.IdleTimeout = TimeSpan.FromMinutes(30);
-    opts.Cookie.IsEssential = true;
-});
-
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseSession();
-app.UseCookiePolicy();
-app.UseMiddleware<ConsentMiddleware>();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error.html");
+    app.UseStaticFiles();
+}
 
-app.MapGet("/session", async context => {
-    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-
-    context.Session.SetInt32("counter1", counter1);
-    context.Session.SetInt32("counter2", counter2);
-
-    await context.Session.CommitAsync();
-
-    await context.Response
-    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-});
-
-app.MapFallback(async context => {
-    await context.Response
-        .WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
-
-    await context.Response.WriteAsync("Hello World!");
+app.Run(context => {
+    throw new Exception("Something has gone wrong");
 });
 
 app.Run();
